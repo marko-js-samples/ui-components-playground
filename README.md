8d086a5acd6be9ed316a261133e5f59947db622e
Austin Kelleher <austin.kell47@gmail.com>
2017-05-17 16:47:53 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+Marko UI Components Playground
+==================================
+
+```bash
+git clone https://github.com/marko-js-samples/ui-components-playground.git
+cd ui-components-playground
+npm install
+npm start
+```
---
+++ components/app-overlay/component.js
@@ -0,0 +1,71 @@
+module.exports = {
+  onInput (input) {
+    this.state = {
+      width: input.width || '80%',
+      visible: input.visible === true,
+      body: input.renderBody
+    };
+  },
+
+  onMount () {
+    this.fixPageScrolling();
+  },
+
+  onUpdate () {
+    this.fixPageScrolling();
+  },
+
+  fixPageScrolling () {
+    if (this.state.visible === true) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  },
+
+  hide () {
+    this.setVisibility(false);
+  },
+
+  show () {
+    this.setVisibility(true);
+  },
+
+  setVisibility (visible) {
+    if (this.state.visible === visible) {
+      // Visibility did not change... nothing to do
+      return;
+    }
+
+    if (visible) {
+      this.emit('show');
+    } else {
+      this.emit('hide');
+    }
+
+    this.setState('visible', visible);
+  },
+
+  handleMaskClick () {
+    this.hide();
+  },
+
+  handleCancelButtonClick () {
+    this.emit('cancel', {});
+    this.hide();
+  },
+
+  handleDoneButtonClick () {
+    var preventDefault = false;
+
+    this.emit('ok', {
+      preventDefault () {
+        preventDefault = true;
+      }
+    });
+
+    if (!preventDefault) {
+      this.hide();
+    }
+  }
+};
